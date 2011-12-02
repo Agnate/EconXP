74d03c2788e8756daad1ed559372e7ce6db36452
Paul Dinelle <pauldinelle@gmail.com>
2011-12-02 17:29:21 -0500

    Added missing .java files from commit.

---
+++ src/ca/agnate/EconXP/OfflineManager.java
@@ -0,0 +1,113 @@
+package ca.agnate.EconXP;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.jnbt.CompoundTag;
+import org.jnbt.FloatTag;
+import org.jnbt.IntTag;
+import org.jnbt.NBTInputStream;
+import org.jnbt.NBTOutputStream;
+import org.jnbt.Tag;
+
+public class OfflineManager {
+	
+	private File dataDir;
+	private EconXP plugin;
+
+    public OfflineManager( EconXP aPlugin ) {
+    	plugin = aPlugin;
+        dataDir = new File(plugin.getServer().getWorlds().get(0).getName(), "players");
+    }
+	
+	public boolean dataExists(String name) {
+        return new File(dataDir, name + ".dat").exists();
+    }
+	
+	public int getBalance(String name) {
+		if (!dataExists(name)) {
+            System.out.println(name+" does not exist.");
+        	return -1;
+        }
+        
+        return readExperience( name );
+    }
+	
+	public boolean setBalance(String name, int value) {
+		if (!dataExists(name)) {
+            System.out.println(name+" does not exist.");
+        	return false;
+        }
+        
+        return writeExperience( name, value );
+    }
+	
+	private int readExperience(String name) {
+        try {
+        	NBTInputStream in = new NBTInputStream( new FileInputStream(new File(dataDir, name + ".dat")) );
+            CompoundTag tag = (CompoundTag) in.readTag();
+            in.close();
+            
+            IntTag exp = (IntTag) tag.getValue().get("XpTotal");
+            
+            return exp.getValue();
+        }
+        catch (IOException ex) {
+            System.out.println("[EconXP] Error reading experience for "+name+"["+ex.getMessage()+"]");
+            return -1;
+        }
+    }
+	
+	private boolean writeExperience(String name, int value) {
+        try {
+        	NBTInputStream in = new NBTInputStream( new FileInputStream(new File(dataDir, name + ".dat")) );
+            CompoundTag tag = (CompoundTag) in.readTag();
+            in.close();
+            
+            // Uncomment this to see the NBT tag list.
+            //for (String aTag : tag.getValue().keySet() ) {
+            //	System.out.println("TEST: "+aTag); 
+            //}
+            
+            // Dirty hacks from Bukkit bugs.
+            if ( value < 0 ) {
+            	value = 0;
+            }
+            
+            int expLevel = 0;
+            float curExp = (float) value / (float) (7 + (expLevel * 7 >> 1));
+            
+            while (curExp >= 1.0F) {
+                --curExp;
+                ++expLevel;
+            }
+            
+            // Create tags for new data here.
+            FloatTag newExpCur = new FloatTag("XpP", curExp);
+            IntTag newExpLevel = new IntTag("XpLevel", expLevel);
+            IntTag newExp = new IntTag("XpTotal", value);
+            
+            HashMap<String, Tag> tagCompound = new HashMap<String, Tag>(tag.getValue());
+            
+            // Add tags here for merging of data.
+            tagCompound.put("XpTotal", newExp);
+            tagCompound.put("XpLevel", newExpLevel);
+            tagCompound.put("XpP", newExpCur);
+            
+            tag = new CompoundTag("Player", tagCompound);
+            
+            NBTOutputStream out = new NBTOutputStream(new FileOutputStream(new File(dataDir, name + ".dat")));
+            out.writeTag(tag);
+            out.close();
+            
+            return true;
+        }
+        catch (IOException ex) {
+            System.out.println("[EconXP] Error reading experience for "+name+"["+ex.getMessage()+"]");
+            return false;
+        }
+    }
+}
